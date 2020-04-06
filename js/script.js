4859d92fc8f69352be2bf1a8eb99c696c846dda7
zhangqi <1729129423@qq.com>
2020-04-06 21:40:45 +0800

    create basic file and init project
    add index.html and images
    
    add style.css
    
    add script.js
    
    add reference

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".queen-1").addEventListener("click", () => {
+  const div = document.createElement("div");
+  div.innerHTML = "fuck you!!!";
+  document.getElementById("app").appendChild(div);
+});
